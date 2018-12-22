b8b2fd44330ce2a02e51ea5b93b10c8e3ffe1a24
Ailton <ailton.msj@gmail.com>
2018-12-22 19:59:12 -0200

    Added Spring Ribbon and improved the service url using rest template

--- src/main/java/br/com/spring/eureka/l4/SpringEurekaSentenceL4Application.java
+++ src/main/java/br/com/spring/eureka/l4/SpringEurekaSentenceL4Application.java
@@ -3,6 +3,9 @@ package br.com.spring.eureka.l4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,13 @@ public class SpringEurekaSentenceL4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEurekaSentenceL4Application.class, args);
+	}
+	
+	// For Spring Ribbon
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/br/com/spring/eureka/l4/controller/DemoController.java
+++ src/main/java/br/com/spring/eureka/l4/controller/DemoController.java
@@ -1,11 +1,6 @@
 package br.com.spring.eureka.l4.controller;
 
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class DemoController {
 	
 	@Autowired
-	DiscoveryClient client;
+	RestTemplate restTemplate;
 	
 	@GetMapping("/")
 	public @ResponseBody String isAlive() {
@@ -34,13 +29,6 @@ public class DemoController {
 	  }
 	  
 	public String getWord(String service) {
-		List<ServiceInstance> list = client.getInstances(service);
-		if (list != null && list.size() > 0) {
-			URI uri = list.get(0).getUri();
-			if (uri != null) {
-				return (new RestTemplate()).getForObject(uri, String.class);
-			}
-		}
-		return null;
+		return restTemplate.getForObject("http://" + service, String.class);
 	}
 }
